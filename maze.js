aa3f2f084b31fce4a0c50b9a0f77299dbbe4785a
norman-ma <norman.michaelanthony@gmail.com>
2016-10-12 17:43:02 -0500

    second task completed

--- maze.js
+++ maze.js
@@ -1,7 +1,24 @@
 window.onload = function(){
-	document.getElementById("boundary1").onmouseover = outOfBounds;
+	//document.getElementById("boundary1").onmouseover = outOfBounds;
+	var bounds = document.querySelectorAll(".boundary");
+	setupBounds(bounds);
+}
+
+
+function setupBounds(list){
+	for(var i = 0; i < list.length; i++ ){
+		list[i].onmouseover = outOfBounds;
+	}
 }
 
 function outOfBounds(){
-	document.getElementById("boundary1").className+=" youlose";
+	//document.getElementById("boundary1").className+=" youlose";
+	var bounds = document.querySelectorAll(".boundary");
+	var c;
+	for(var i = 0; i < bounds.length; i++ ){
+		c = bounds[i].className;
+		if(c === "boundary"){
+			bounds[i].className+=" youlose";
+		}
+	}	
 }
