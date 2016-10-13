d356be3240ca2cb6a3b1074d14b243a2a25578fe
norman-ma <norman.michaelanthony@gmail.com>
2016-10-12 22:55:48 -0500

    final task completed

--- maze.js
+++ maze.js
@@ -1,15 +1,20 @@
-var bounds;
+var bounds,obounds;
 var s = false;
 var w = false;
 
 window.onload = function(){
 	//document.getElementById("boundary1").onmouseover = outOfBounds;
 	bounds = document.querySelectorAll(".boundary");
-	
+	obounds = defOBounds();
 	setupBounds(bounds);
 	document.getElementById("start").onmouseover = start;
 	document.getElementById("end").onmouseover = win;	
 	document.getElementById("start").onclick = reset;
+	document.onmousemove = checkBounds;
+}
+
+window.onresize = function(){
+	obounds = defOBounds();
 }
 
 function setupBounds(list){
@@ -46,10 +51,38 @@ function win(){
 }
 
 function reset(){
+	var c;
 	for(var i = 0; i < bounds.length; i++ ){
-		bounds[i].className = "boundary";
+		c = bounds[i].className;
+		if (c === "boundary youlose"){
+			bounds[i].className = "boundary";
+		}
 	}	 
 	document.getElementById("status").textContent = "Move your mouse over the \"S\" to begin.";
 	w = false;
-	s = false;
+	s = true;
+}
+
+function defOBounds(){
+	var m = document.getElementById("maze");
+	
+	var l = m.offsetLeft;
+	var t = m.offsetTop;
+	var r = m.offsetLeft + m.offsetWidth;
+	var b = m.offsetTop + m.offsetHeight;
+	console.log(l,r,t,b);
+	return [l,r,t,b];
+}
+
+function inBounds(p){
+	return p[0] >= obounds[0] && p[0] <= obounds[1] && p[1] >= obounds[2] && p[1] <= obounds[3];
+}
+
+function checkBounds(event){
+	event = event || window.event;
+	//console.log(event.clientX,event.clientY);
+	var pos = [event.clientX,event.clientY]
+	if(s && !inBounds(pos)){		
+		outOfBounds();
+	}
 }
